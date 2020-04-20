c3f44fe49c9e992b4a257466d521bfb6284c8f91
Gyurci <lgyurci@gmail.com>
2020-04-21 00:24:58 +0200

    way to almost go

--- src/read.cpp
+++ src/read.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <libusb-1.0/libusb.h>
+#include <thread>
+#include <chrono>
 #define dven 4292
 #define dprod 60001
 
@@ -25,6 +27,10 @@ struct confdata {
     int alarm_low2 = 1086324736;
     int alarm_high2 = 1106247680;
     const int endsignal = 206;
+};
+struct b3header{
+    char id;
+    short int dat;
 };
 
 int main(){
@@ -67,10 +73,38 @@ int main(){
     confdata conf;
     unsigned char confsignal[3];
     confsignal[0]=0;
+    confsignal[1]=16;
+    confsignal[2]=1;
+    libusb_bulk_transfer(dev_handle,2,confsignal,3,&written,0);
+    libusb_bulk_transfer(dev_handle,130,confsignal,3,&written,0);
+    cout << "num data header received: " << (int) confsignal[0] << " " << (int) confsignal[1] << " " << (int) confsignal[2] << " " << endl;
+    confdata liveconfig;
+    libusb_bulk_transfer(dev_handle,130,(unsigned char*)&liveconfig,64,&written,0);
+
+    //std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    libusb_release_interface(dev_handle,0);
+    libusb_close(dev_handle);
+    libusb_exit(ctx);
+    libusb_init(&ctx);
+    dev_handle = libusb_open_device_with_vid_pid(ctx,dven,dprod);
+        if (libusb_kernel_driver_active(dev_handle,0) == 1){
+        cout << "Detaching kernel driver" << endl;
+        if (libusb_detach_kernel_driver(dev_handle,0) == 0){
+            cout << "Detached." << endl;
+        }
+    }
+    if (libusb_claim_interface(dev_handle,0) < 0){
+        cout << "cannot claim interface" << endl;
+    }
+    libusb_control_transfer(dev_handle,64,0,65535,0,NULL,0,0);
+    libusb_control_transfer(dev_handle,64,2,2,0,NULL,0,0);
+    confsignal[0]=0;
     confsignal[1]=0;
     confsignal[2]=1;
     libusb_bulk_transfer(dev_handle,2,confsignal,3,&written,0);
     short int rdt[32];
+    libusb_bulk_transfer(dev_handle,130,confsignal,3,&written,0);
+//    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
     libusb_bulk_transfer(dev_handle,130,confsignal,3,&written,0);
     cout << "header received: " << (int) confsignal[0] << " " << (int) confsignal[1] << " " << (int) confsignal[2] << " " << endl;
     libusb_bulk_transfer(dev_handle,130,(unsigned char*) &rdt,64,&written,0);
@@ -79,10 +113,8 @@ int main(){
         cout << rdt[i] << " ";
     }
     cout << endl;
-    libusb_bulk_transfer(dev_handle,130,(unsigned char*) &rdt,64,&written,0);
-    cout << written << " bytes were read:" << endl;
-    for (int i = 0; i < written/2; i++){
-        cout << rdt[i] << " ";
-    }
-    cout << endl;
+    libusb_control_transfer(dev_handle,64,2,4,0,NULL,0,0);
+    libusb_release_interface(dev_handle,0);
+    libusb_close(dev_handle);
+    libusb_exit(ctx);
 }
